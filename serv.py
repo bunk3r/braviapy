ea204b03db87c7d0e4c6bfe35bbc968b4a9ab018
bunk3r <andrea.purificato@gmail.com>
2015-07-21 19:52:01 +0200

    serv on port 8000 for SUBSCRIBE callback

---
+++ serv.py
@@ -0,0 +1,38 @@
+from BaseHTTPServer import BaseHTTPRequestHandler
+import urlparse
+
+class GetHandler(BaseHTTPRequestHandler):
+
+    def do_NOTIFY(self):
+        parsed_path = urlparse.urlparse(self.path)
+        message_parts = [
+                'CLIENT VALUES:',
+                'client_address=%s (%s)' % (self.client_address,
+                                            self.address_string()),
+                'command=%s' % self.command,
+                'path=%s' % self.path,
+                'real path=%s' % parsed_path.path,
+                'query=%s' % parsed_path.query,
+                'request_version=%s' % self.request_version,
+                '',
+                'SERVER VALUES:',
+                'server_version=%s' % self.server_version,
+                'sys_version=%s' % self.sys_version,
+                'protocol_version=%s' % self.protocol_version,
+                '',
+                'HEADERS RECEIVED:',
+                ]
+        for name, value in sorted(self.headers.items()):
+            message_parts.append('%s=%s' % (name, value.rstrip()))
+        message_parts.append('')
+        message = '\r\n'.join(message_parts)
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(message)
+        return
+
+if __name__ == '__main__':
+    from BaseHTTPServer import HTTPServer
+    server = HTTPServer(('', 8000), GetHandler)
+    print 'Starting server, use <Ctrl-C> to stop'
+    server.serve_forever()
